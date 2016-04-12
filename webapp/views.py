3a4b8eeae51cba23a05486a09aec4df0d17f1cad
FRANK <you@example.com>
2016-04-12 11:05:53 +0200

    removing Webapp

--- webapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('<h1> HEY! AM BACK AGAIN</h1>')
-
-def goo(request):
-    return HttpResponse('Are we cool!!')
